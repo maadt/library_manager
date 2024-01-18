d688a81d01e8f5581c9dfd5b9b6e1edf650b98f4
maadt <147260993+maadt@users.noreply.github.com>
2024-01-18 11:34:48 +0900

    Lesson02実装完了

---
+++ src/main/java/com/example/repository/LibraryRepository.java
@@ -0,0 +1,16 @@
+package com.example.repository;
+
+// JpaRepositoryを拡張してデータベースのCRUD操作を行うインターフェース
+// サービスクラスに機能を提供する
+// エンティティクラスとデータベースを正しくマッピングする
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.entity.Library;
+
+@Repository
+public interface LibraryRepository extends JpaRepository<Library, Integer> {
+// <エンティティクラス, 主キー>とすることでエンティティクラスとデータベースが正しくマッピングされる
+	
+}
